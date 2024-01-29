ce5ede5a53c15fad49deb627d4292f6758d2a140
saidmeza <176p0393@itspozarica.edu.mx>
2024-01-29 14:23:50 -0600

    firt commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const boton = document.getElementById("boton");
+
+const fondo = document.getElementById("fondo");
+
+let contador = 0;
+
+
+
+
+
+// boton.addEventListener("click",()=>{
+    
+//     if(contador == 0 ) 
+//     {
+//         fondo.className = ("backgraound show")  
+//         contador = 1;
+//     }
+//     else
+//     {
+//         fondo.classList.remove("noshow");
+//         fondo.className = ("backgraound noshow");
+//         contador = 0;
+//     }
+    
+//     })
